dfb9d3e048229c85ae70816464dec51d691d9823
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-10 16:43:46 -0300

    first commit

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,40 +1,14 @@
+import {airtableData} from '@/lib/airtable';
 import Airtable from 'airtable';
+import {NextResponse} from 'next/server';
 export async function GET() {
   const base = new Airtable({apiKey: process.env.AIRTABLE}).base(
     'appXu0aYFo1OsZRi0'
   );
   try {
-    let newRecords: any[] = [];
-    await new Promise<void>((resolve, reject) => {
-      base('Furniture')
-        .select({
-          pageSize: 10,
-        })
-        .eachPage(
-          function page(records, fetchNextPage) {
-            records.map(function (record: any) {
-              newRecords.push({
-                Name: record.fields.Name,
-                img: record.fields.Images[0].url,
-                price: record.fields['Unit cost'],
-                id: record.id,
-              });
-            });
-            fetchNextPage();
-          },
-          function done(err) {
-            if (err) {
-              console.error('Error', err);
-              reject(err); // Rechaza la promesa en caso de error
-              return;
-            }
-            resolve(); // Resuelve la promesa cuando la operación se completa
-          }
-        );
-    });
-    return Response.json(newRecords);
+    const data = await airtableData();
+    return NextResponse.json(data, {status: 200});
   } catch (e) {
-    console.log(e);
     return Response.json(e);
   }
 }
---
+++ lib/airtable.ts
@@ -0,0 +1,36 @@
+import Airtable from 'airtable';
+export async function airtableData() {
+  return new Promise<any[]>((resolve, reject) => {
+    const base = new Airtable({apiKey: process.env.AIRTABLE}).base(
+      'appXu0aYFo1OsZRi0'
+    );
+
+    let newRecords: any[] = [];
+
+    base('Furniture')
+      .select({
+        pageSize: 10,
+        view: 'All furniture',
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function (record: any) {
+            newRecords.push({
+              Name: record.fields.Name,
+              img: record.fields.Images[0].url,
+              price: record.fields['Unit cost'],
+              id: record.id,
+            });
+          });
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(newRecords);
+        }
+      );
+  });
+}
