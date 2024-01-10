60d3bbd1022edf529e624fb6c69a9243f312029a
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-10 17:29:48 -0300

    mejorando

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,10 +1,6 @@
 import {airtableData} from '@/lib/airtable';
-import Airtable from 'airtable';
 import {NextResponse} from 'next/server';
 export async function GET() {
-  const base = new Airtable({apiKey: process.env.AIRTABLE}).base(
-    'appXu0aYFo1OsZRi0'
-  );
   try {
     const data = await airtableData();
     return NextResponse.json(data, {status: 200});
