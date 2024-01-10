b53a7deeedf4ec94e974327e6e7bdd1346c633ce
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-10 18:29:12 -0300

    mejorando

--- components/productos.tsx
+++ components/productos.tsx
@@ -1,38 +1,39 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 export async function ProductosComponent() {
   try {
-    const response = await fetch(
-      process.env.API || 'http://localhost:3000/api/products',
-      {
-        cache: 'no-cache',
-      }
-    );
+    // const response = await fetch(
+    //   process.env.API || 'http://localhost:3000/api/products',
+    //   {
+    //     cache: 'no-cache',
+    //   }
+    // );
 
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    }
+    // if (!response.ok) {
+    //   throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    // }
 
-    const data = await response.json();
-
+    // const data = await response.json();
+    const data: any = [];
     return (
       <div className='flex justify-center flex-wrap gap-8 m-8 max-md:m-2'>
-        {data.map((item: any) => (
-          <div
-            key={item.id}
-            className='flex gap-4 w-[350px] items-center  bg-[#ffefa9] rounded-lg'>
-            <img
-              src={item.img}
-              alt={item.Name}
-              className='w-[100px] h-full object-cover'
-              loading='lazy'
-            />
-            <div className='flex flex-col gap-4'>
-              <h2>{item.Name || 'prueba'} </h2>
-              <p className='font-bold'>${item.price}</p>
+        {data.length &&
+          data.map((item: any) => (
+            <div
+              key={item.id}
+              className='flex gap-4 w-[350px] items-center  bg-[#ffefa9] rounded-lg'>
+              <Image
+                src={item.img}
+                alt={item.Name}
+                className='w-[100px] h-full object-cover'
+                loading='lazy'
+              />
+              <div className='flex flex-col gap-4'>
+                <h2>{item.Name || 'prueba'} </h2>
+                <p className='font-bold'>${item.price}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         <div className='fixed bottom-5 right-5 z-9 bg-[#40ea41] rounded-full p-2 shadow-[0_0_10px_0.5px] hover:opacity-80'>
           <Link href='https://api.whatsapp.com/send?phone=+541159102865&text=Hola%20te%20hablo%20desde%20la%20p%C3%A1gina'>
             <svg
