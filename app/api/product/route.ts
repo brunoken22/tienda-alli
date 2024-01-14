7438d470f9171ecbd004489765538eb2635aba6c
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-14 13:59:23 -0300

    mejorandoi

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -2,7 +2,11 @@ import {index} from '@/lib/algolia';
 import {NextResponse} from 'next/server';
 export async function GET() {
   try {
-    const data = await index.search('');
+    const data = await index.search('', {
+      page: 0, // Número de página
+      hitsPerPage: 50,
+    });
+    console.log(data.hits.length);
     return NextResponse.json(data.hits);
   } catch (e) {
     return NextResponse.json(e);
