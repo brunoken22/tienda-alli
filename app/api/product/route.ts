e7d74fd695bb04bebc9857efcd701c2796246400
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-14 12:40:16 -0300

    data string

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -2,7 +2,7 @@ import {airtableData} from '@/lib/airtable';
 import {NextResponse} from 'next/server';
 export async function GET() {
   try {
-    const data = await airtableData();
+    const data = 'await airtableData()';
     return NextResponse.json(data);
   } catch (e) {
     return NextResponse.json(e);
