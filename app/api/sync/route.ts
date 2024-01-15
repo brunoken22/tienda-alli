ab63fa464fc044c038bd3aa00532bc0e7258e8e9
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-15 15:37:13 -0300

    mejorando

--- app/api/sync/route.ts
+++ app/api/sync/route.ts
@@ -1,6 +1,21 @@
-import {getDataAirtable} from '@/lib/airtable';
+import {base} from '@/lib/airtable';
+import {index} from '@/lib/algolia';
+
+// import {getDataAirtable} from '@/lib/airtable';
 export async function GET() {
   const data = await getDataAirtable();
   console.log('GET', data);
   return Response.json(data);
+}
+export async function getDataAirtable() {
+  const newBase = base('Furniture').select({
+    pageSize: 100,
+  });
+  const response = await newBase.all();
+  const object = response.map((r) => ({
+    objectID: r.id,
+    ...r.fields,
+  }));
+  await index.saveObjects(object);
+  return object.length;
 }
