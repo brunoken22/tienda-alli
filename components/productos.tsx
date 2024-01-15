653c794c09d60712cc446116bc74b92cfc7a6bae
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-15 08:45:01 -0300

    mejorando

--- components/productos.tsx
+++ components/productos.tsx
@@ -81,7 +81,7 @@ export function ProductosComponent() {
       </div>
       <Link
         href={'https://www.facebook.com/marketplace/profile/100025099413594/'}
-        className='flex justify-center gap-4 items-center mt-8 flex-wrap'>
+        className='flex justify-center gap-4 items-center mt-12 flex-wrap mb-8'>
         <Image src={'/facebook.svg'} width={100} height={100} alt='facebook' />
         <h2 className='text-3xl font-bold max-md:text-center'>
           VISÍTANOS <br></br>EN NUESTRO<br></br> MARKETPLACE
