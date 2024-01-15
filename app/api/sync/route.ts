5e21abcf0c3420f5994c56a84eed9c72798cf91f
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-15 15:31:11 -0300

    mejorando

--- app/api/sync/route.ts
+++ app/api/sync/route.ts
@@ -1,32 +1,6 @@
-import {base} from '@/lib/airtable';
-import {index} from '@/lib/algolia';
+import {getDataAirtable} from '@/lib/airtable';
 export async function GET() {
-  base('Furniture')
-    .select({
-      pageSize: 100,
-    })
-    .eachPage(
-      async function page(records, fetchNextPage) {
-        const object = records.map((r) => {
-          return {
-            objectID: r.id,
-            ...r.fields,
-          };
-        });
-        console.log(object.length);
-
-        await index.saveObjects(object);
-        fetchNextPage();
-      },
-      function done(err) {
-        if (err) {
-          return;
-        }
-        console.log('TERMINADO');
-      }
-    );
-  // const data: any = await getDataAirtable();
-  // console.log('GET', data.length);
-
-  return Response.json('data.length');
+  const data: any = await getDataAirtable();
+  console.log('GET', data);
+  return Response.json(data);
 }
--- lib/airtable.ts
+++ lib/airtable.ts
@@ -4,39 +4,14 @@ export const base = new Airtable({apiKey: process.env.AIRTABLE}).base(
   'appXu0aYFo1OsZRi0'
 );
 export async function getDataAirtable() {
-  const newProduct: any = [];
-
-  return new Promise((resolve, reject) => {
-    base('Furniture')
-      .select({
-        pageSize: 100,
-      })
-      .eachPage(
-        async function page(records, fetchNextPage) {
-          const object = records.map((r) => ({
-            objectID: r.id,
-            ...r.fields,
-          }));
-
-          // Add the objects to the newProduct array
-          newProduct.push(...object);
-
-          fetchNextPage();
-        },
-        async function done(err) {
-          if (err) {
-            reject(err); // Reject the promise if there is an error
-            return;
-          }
-
-          try {
-            // Save all objects to the index in one call after fetching all pages
-            await index.saveObjects(newProduct);
-            resolve(newProduct); // Resolve the promise with the newProduct array
-          } catch (saveError) {
-            reject(saveError); // Reject the promise if there is an error during saving
-          }
-        }
-      );
+  const newBase = base('Furniture').select({
+    pageSize: 100,
   });
+  const response = await newBase.all();
+  const object = response.map((r) => ({
+    objectID: r.id,
+    ...r.fields,
+  }));
+  await index.saveObjects(object);
+  return object.length;
 }
