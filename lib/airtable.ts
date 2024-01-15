30ca3ca4dba7bd7dc743b90a7e75a33196922877
Bruno Ken <bruno_am_22@hotmail.com>
2024-01-15 14:54:47 -0300

    regresan

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -1,6 +1,6 @@
 import Airtable from 'airtable';
 import {index} from './algolia';
-const base = new Airtable({apiKey: process.env.AIRTABLE}).base(
+export const base = new Airtable({apiKey: process.env.AIRTABLE}).base(
   'appXu0aYFo1OsZRi0'
 );
 export async function getDataAirtable() {
