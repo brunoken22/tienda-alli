ac9f80a71734385f6786d59b867506d67fdb67c7
Bruno Ken <bruno_am_22@hotmail.com>
2024-02-08 22:53:52 -0300

    mejorando img carousel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -31,7 +31,7 @@ export function CarouselHeader() {
         pagination={{
           clickable: true,
         }}
-        modules={[Autoplay, Pagination]}
+        modules={[Pagination]}
         onAutoplayTimeLeft={onAutoplayTimeLeft}
         className='mySwiper'>
         {data &&
