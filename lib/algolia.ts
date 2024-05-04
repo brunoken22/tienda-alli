e16ad283c0b2685b223237354e47203e777576d9
Bruno Ken <bruno_am_22@hotmail.com>
2024-05-04 11:26:36 -0300

    mejorando

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -1,9 +1,4 @@
 import algoliasearch from 'algoliasearch';
-// const client = algoliasearch('M5BJRFSVUS', process.env.ALGOLIA as string);
-const clientRposteria = algoliasearch(
-  'NRDCDSL7R8',
-  process.env.ALGOLIA as string
-);
+const client = algoliasearch('M5BJRFSVUS', process.env.ALGOLIA as string);
 
-// export const index = client.initIndex('product');
-export const index = clientRposteria.initIndex('products');
+export const index = client.initIndex('product');
