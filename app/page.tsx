91246010884b26048ac449521c8108cd9b328ed5
Bruno Ken <bruno_am_22@hotmail.com>
2024-07-29 11:21:16 -0300

    mejroando app

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import {CarouselHeader} from '@/components/carousel';
 import {ProductsFeatured} from '@/components/featured';
 import {getFrontPage, getProductFeatured} from '@/lib/hook';
@@ -5,7 +6,6 @@ import Link from 'next/link';
 export default async function Home() {
   const data = await getFrontPage();
   const featured = await getProductFeatured();
-
   return (
     <div className='m-auto max-md:mt-[6.5rem] mt-[4rem] max-w-[1200px] shadow-[0_0_100px_10px_#3c006c]'>
       <div className={`h-[400px]  `}>
